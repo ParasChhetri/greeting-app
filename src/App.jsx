6110febd46bfbea383ae317ce830e78cab7983b9
ParasChhetri <paraschhetri477@gmail.com>
2025-01-08 00:27:56 +0530

    Added my own name

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,44 @@ import './App.css'
 import { useEffect } from 'react';
 
 function App() {
-  const [name,setName] = useState("");
-  const [submittedName,setSubmittedName] = useState("");
-  const [greetingWithTime,setGreetingWithTime] = useState("");
-  const [bgColor,setBgColor] = useState("bg-gray-200")
-  const [time,setTime] = useState(new Date());
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+  const [greetingWithTime, setGreetingWithTime] = useState("");
+  const [bgColor, setBgColor] = useState("bg-gray-200")
+  const [time, setTime] = useState(new Date());
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date());
-    },1000);
+    }, 1000);
     // used to clean up the memory
     return () => clearInterval(timer);
-  },[]);
+  }, []);
 
   // Good morning, Good afternoon, Good evening, Good night and also added background color change with time
   useEffect(() => {
     const currentHour = time.getHours(); // only to avoid scope bugs
-    if(currentHour < 5) {
+    if (currentHour < 5) {
       setGreetingWithTime("Good night");
       setBgColor("bg-gray-900 text-white");
-    }else if(currentHour < 12) {
+    } else if (currentHour < 12) {
       setGreetingWithTime("Good morning");
       setBgColor("bg-yellow-100");
-    }else if(currentHour < 17) {
+    } else if (currentHour < 17) {
       setGreetingWithTime("Good Afternoon");
       setBgColor("bg-blue-200");
-    }else if(currentHour < 20) {
+    } else if (currentHour < 20) {
       setGreetingWithTime("Good evening");
       setBgColor("bg-orange-300");
-    }else {
+    } else {
       setGreetingWithTime("Good night");
       setBgColor("bg-gray-900 text-white");
     }
   }, [time]);
 
   // AM to PM format
-  const hours = time.getHours();
-  const minutes = time.getMinutes().toString().padStart(2,"0");
-  const seconds = time.getSeconds().toString().padStart(2,"0");
+  const hours = time.getHours().toString().padStart(2, "0");
+  const minutes = time.getMinutes().toString().padStart(2, "0");
+  const seconds = time.getSeconds().toString().padStart(2, "0");
 
   // handel cahnges with input
   const handelChange = (event) => {
@@ -62,8 +62,8 @@ function App() {
       <div className={`min-h-[100vh] flex flex-col px-10 pt-5 ${bgColor}`}>
         <h1 className='text-center text-3xl my-9'>Greeting App</h1>
         <h2>Current Time: <span>{`${hours}:${minutes}:${seconds} ${hours >= 12 ? "PM" : "AM"}`}</span></h2>
-        
-        <input type="text" placeholder='enter your name' value={name} onChange={handelChange} className='border w-96 px-4 py-2 rounded-lg my-4 dark:text-gray-700'/>
+
+        <input type="text" placeholder='enter your name' value={name} onChange={handelChange} className='border w-96 px-4 py-2 rounded-lg my-4 dark:text-gray-700' />
         <div className='min-h-14 my-2'>
           {
             submittedName && (
@@ -74,6 +74,9 @@ function App() {
         <div className='flex justify-between items-center'>
           <button className='px-4 py-2 bg-white font-bold text-lg rounded-lg shadow-xl active:-translate-y-1 dark:text-black' onClick={handelSubmitt}>Click to generate</button>
           <button className='px-4 py-2 bg-red-400 font-bold text-lg rounded-lg shadow-xl active:-translate-y-1' onClick={resetButton}>Reset</button>
+        </div>
+        <div className='my-8 text-center'>
+          Made my Paras Chhetri
         </div>
       </div>
     </>
